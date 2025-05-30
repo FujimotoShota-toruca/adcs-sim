f157b08c817a53ac3f927bfb5d3bb14dca7b5b49
FujimotoShota-toruca <fujimoto.shota.0401.0304@gmail.com>
2025-05-30 21:12:55 +0900

    余計なfileを削除

--- design.py
+++
@@ -1,7 +0,0 @@
-from exit_output import discord
-
-if __name__ == "__main__":
-    discord_utility = discord.DiscordSendMessage()
-
-    # 完了通知をDiscordへ送信
-    discord_utility.send_message("通知のテストです。")
